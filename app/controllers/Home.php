1eabe7177341e331fb5e2e1300209e99ab75735f
Diana Sinenkova <drsinenkova@gmail.com>
2023-04-07 00:53:45 +0400

    on hosting

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -2,17 +2,19 @@
 class Home extends Controller {
     public function index() {
         $data = [];
-        
+
         if(isset($_POST['name'])) {
             $message = $this->model('FormModel');
             $message->setData($_POST['name'], $_POST['phone'], $_POST['email'], $_POST['message']);
-
             $isValid = $message->validForm();
-            if ($isValid == "Сообщение отправлено")
+            if ($isValid == "Сообщение отправлено") {
                 $message->addMessage();
-            else
                 $data['message'] = $isValid;
+            } else {
+                $data['message'] = $isValid;
+            }
         }
+
 
         $this->view('home/index', $data);
     }
